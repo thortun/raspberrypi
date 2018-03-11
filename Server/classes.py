3f0ae405b21b9cbe0bf78b670761cf6154b28835
Thor <thortun@stud.ntnu.no>
2018-03-11 17:40:04 +0000

    More changes

--- Server/classes.py
+++ Server/classes.py
@@ -2,27 +2,15 @@ import socket
 import hashlib
 
 import _globals
-
-class Request():
-    """Class to handle which requests are sent to my server."""
-    def __init__(self, type):
-        """Initializes the instance."""
-        self.type = type                       # Initialize the request type
         
-        
-        
-            
 class Server():
     """Simple server class."""
     def __init__(self, port = _globals.PORT, pwd = 1234):
         """Initializes the server with a port number."""
         self.s = socket.socket()               # Initialize the socket
-        self.host = '192.168.1.10'             # Host in static
+        self.host = _globals.HOST              # Host in static
         self.port = port                       # Initialize port, defaults to 9999
-        try:
-            self.s.bind((self.host, self.port)) # Bind the socket
-        except socket.error:
-            self.port += (self.port + 1) % 10000 # Try a different port 
+        self.s.bind((self.host, self.port))    # Bind the socket
         self.s.listen(5)                       # Start listening
         
         m = hashlib.sha256()                   # Start hashing
@@ -36,10 +24,10 @@ class Server():
             
             # Now listen for a request
             request = c.recv(1024)             # This is the request-code we got
-            print(object(request))
-            print(object(_globals.REQUESTDICT['talk']))
-            print(request == _globals.REQUESTDICT['talk'])
-            self.handleRequest(c, request)
+            try:
+                self.handleRequest(c, int(request))# Now handle what happens due to this request
+            except ValueError: # This happens if the client does not request anything
+                print('Invalid request, client did not send any request?')
 
             c.close()                          # Close the client when we are done
         
@@ -50,6 +38,43 @@ class Server():
     def handleRequest(self, client, request):
         """Handles a reqest."""
         if request == 0: # We just want to talk
-            client.send('You wanted to talk?'.encode('utf-8'))
+            client.send("You wanted to talk?".encode('utf-8'))
+        elif request == _globals.REQUESTDICT['file']:    # Requesting a file
+            pass
+        elif request == _globals.REQUESTDICT['weather']: # Requested weather data
+            # Begin by opening the current weather data file
+            fileID = open('/home/pi/Documents/Python/Server/Data/weatherData.txt', 'r') # Open for reading
+            data = fileID.read()  # Read the data
+            client.send(data.encode('utf-8')) # Send the correctly encoded data
+            
         else:
-            print('Unknown request')        
+            print('Unknown request. Undefined request code.')
+            
+class Client:
+    """Client class to make handeling various requests easier."""
+    def __init__(self, port = _globals.PORT):
+        """Initializes the client."""
+        self.s = socket.socket()   # Create a socket instance
+        self.host = _globals.HOST  # Set the host
+        self.port = port           # Set the port
+        self.s.connect((self.host, self.port)) # Connect to the server
+        print(self.s.recv(1024))   # Print whatever the server sends, such as confirmation
+        
+    def sendRequest(self, request):
+        """Handles requests."""
+        if request == _globals.REQUESTDICT['talk']:      # We just want to talk
+            pass
+        elif request == _globals.REQUESTDICT['file']:    # Request a file
+            pass
+        elif request == _globals.REQUESTDICT['weather']: # Request weather data
+            self.s.send(str(request).encode('utf-8'))    # Send the correct request
+            data = self.s.recv(1024)                     # Receive the first data block
+            weatherData = ''                             # Make a variable to store all the data
+            while data:                                  # While there are still data to recieve
+                weatherData += data                      # Append the new block to the weather data
+                data = self.s.recv(1024)                 # Store the next block of data
+            
+
+        
+        
+        
--- Server/client.py
+++ Server/client.py
@@ -6,19 +6,8 @@ import _globals
 
 def main():
 
-    s = socket.socket()         # Create a socket object
-    host = '192.168.1.10'       # Get local machine ip
-    port = _globals.PORT                 # Reserve a port for your service.
-
-    print(s.recv(1024).decode('utf-8'))
-    
-    # Now wend a request
-    request = 'talk'
-    s.send(bytes(_globals.REQUESTDICT[request]))
-    
-    # Close the socket with the server
-    s.close()
-    
+    c = classes.Client()
+    c.sendRequest(2)
 
 if __name__ == '__main__':
     main()
