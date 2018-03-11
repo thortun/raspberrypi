dd212f8081c1b1ca197e85ab9c589ddca2cee840
Thor <thortun@stud.ntnu.no>
2018-03-11 19:02:17 +0000

    More

---
+++ Server/_globals.py
@@ -0,0 +1,9 @@
+"""Global variables used by many of the other modules."""
+
+REQUESTDICT = {}
+REQUESTDICT['talk'] = 0         # Dummy request code
+REQUESTDICT['file'] = 1         # Request a file
+REQUESTDICT['weather'] = 2      # Request current weather data
+
+HOST = '192.168.1.10'
+PORT = 9099
